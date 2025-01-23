4a901b6ad2189c04e115bdcaf9cfd3ae1de3a6b9
parthsahani5@gmail.com <parthsahani@Parths-MacBook-Air.local>
2025-01-23 15:26:59 +0530

    Resolve merge conflict in taskManager.js

--- taskManager.js
+++ taskManager.js
@@ -1,3 +1,15 @@
 function taskSummary() {
     console.log('Summary of tasks');
 }
+
+function deleteTask(taskId) {
+    tasks = tasks.filter(t => t.id !== taskId);
+}
+
+function addTask(task) {
+    tasks.push(task);
+}
+
+function taskSummary() {
+    console.log('Summary of tasks');
+}
